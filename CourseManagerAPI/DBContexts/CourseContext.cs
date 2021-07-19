73304da52129668e0aff2bef63eda30604ae4ac3
jianyik96 <jianyi96kang@gmail.com>
2021-07-19 21:15:58 +0800

    Add project files.

---
+++ CourseManagerAPI/DBContexts/CourseContext.cs
@@ -0,0 +1,26 @@
+﻿using CourseManagerAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseManagerAPI.DBContexts
+{
+    public class CourseContext: DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public CourseContext()
+        { }
+
+        public CourseContext(DbContextOptions<CourseContext> options)
+            : base(options)
+        {
+        }
+    }
+}
